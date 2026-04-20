b72fd1bd10589a454f4f623c0a0aee3d4bb592d2
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-20 22:30:22 +0600

    Update cluster.js

--- backend/src/cluster.js
+++ backend/src/cluster.js
@@ -1,1 +1,23 @@
+const cluster = require("cluster");
+const os = require("os");
 
+const totalCPUs = os.cpus().length;
+
+if (cluster.isPrimary) {
+  console.log(`Master process ${process.pid} running`);
+
+  // CPU core অনুযায়ী worker তৈরি
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
+  }
+
+  // worker মারা গেলে নতুন worker চালু
+  cluster.on("exit", (worker) => {
+    console.log(`Worker ${worker.process.pid} died. Restarting...`);
+    cluster.fork();
+  });
+
+} else {
+  // Worker process → server run
+  require("./server");
+}
