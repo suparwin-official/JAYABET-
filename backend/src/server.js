b536c02534b6a832afbf55101c7896a277d8336d
suparwin-official <gorommasalahitbd@gmail.com>
2026-04-20 22:27:05 +0600

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
 
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join_game", (data) => {
+    socket.join(data.room);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Backend running on port 3000");
+});
